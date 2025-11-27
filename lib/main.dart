faefec17e053254dc340cec767333bebf01f527d
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-11-27 15:10:25 +0900

    fix the directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wo_read/Hiragana.dart';
+import 'package:wo_read/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HiraganaPage(),
+      home: HomePage(),
     );
   }
 }
