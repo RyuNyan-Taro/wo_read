af12646f0b8e9f73ca8b8d9d00ade3e920b2a738
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-06 21:14:07 +0900

    add LunarAge class

---
+++ lib/record/models/lunar_age.dart
@@ -0,0 +1,6 @@
+class LunarAge {
+  final int year;
+  final int month;
+
+  const LunarAge({required this.year, required this.month});
+}
--- test/record/use_cases/lunar_age_test.dart
+++ test/record/use_cases/lunar_age_test.dart
@@ -1,19 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:parameterized_test/parameterized_test.dart';
+import 'package:wo_read/record/models/lunar_age.dart';
 
 void main() {
   parameterizedTest(
     'input DateTime is converted to lunar age',
     [
-      [DateTime(2021, 6, 1), LunarAge(0, 0)],
-      [DateTime(2022, 5, 31), LunarAge(1, 0)],
-      [DateTime(2022, 2, 28), LunarAge(0, 9)],
-      [DateTime(2021, 5, 31), LunarAge(0, 0)],
+      [DateTime(2021, 6, 1), LunarAge(year: 0, month: 0)],
+      [DateTime(2022, 5, 31), LunarAge(year: 1, month: 0)],
+      [DateTime(2022, 2, 28), LunarAge(year: 0, month: 9)],
+      [DateTime(2021, 5, 31), LunarAge(year: 0, month: 0)],
     ],
     (DateTime datetime, LunarAge expect) {
       final DateTime birthDay = DateTime(2021, 6, 1);
-      final result = convertToLunarAge(dateTime: datetime, birthDay: birthDay);
-      expect(result, expect);
+      final LunarAge result = convertToLunarAge(
+        dateTime: datetime,
+        birthDay: birthDay,
+      );
+
+      expect(result.year, expect.year);
+      expect(result.month, expect.month);
     },
   );
 }
