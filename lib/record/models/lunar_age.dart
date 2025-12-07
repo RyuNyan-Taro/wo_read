ee228598808208790e6bb67e7e0e9205e1af86b4
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-07 09:13:15 +0900

    modify to add equatable

--- lib/record/models/lunar_age.dart
+++ lib/record/models/lunar_age.dart
@@ -1,6 +1,11 @@
-class LunarAge {
+import 'package:equatable/equatable.dart';
+
+class LunarAge extends Equatable {
   final int year;
   final int month;
 
   const LunarAge({required this.year, required this.month});
+
+  @override
+  List<Object?> get props => [year, month];
 }
