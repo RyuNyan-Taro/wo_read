fb8a68934400979623c3ac0c3e28502ed8f22b64
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-09 21:25:27 +0900

    modify to use list of MovingShape

---
+++ lib/shape_move/screens/moving_shape.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MovingShape extends StatefulWidget {
+  final Offset position;
+  const MovingShape({super.key, required this.position});
+
+  @override
+  _MovingShapeState createState() => _MovingShapeState();
+}
+
+class _MovingShapeState extends State<MovingShape> {
+  static const double headerMargin = 80;
+
+  late Offset position;
+
+  @override
+  void initState() {
+    super.initState();
+    position = widget.position;
+  }
+
+  void _onPanUpdate(
+    DragUpdateDetails details,
+    Size widgetSize,
+    double shapeWidth,
+    shapeHeight,
+  ) {
+    final double xPos = max(0, position.dx + details.delta.dx);
+    final double yPos = max(0, position.dy + details.delta.dy);
+
+    setState(() {
+      position = Offset(
+        xPos + shapeWidth <= widgetSize.width ? xPos : position.dx,
+        yPos + shapeHeight + headerMargin <= widgetSize.height
+            ? yPos
+            : position.dy,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size widgetSize = MediaQuery.of(context).size;
+
+    return Positioned(
+      left: position.dx,
+      top: position.dy,
+      child: GestureDetector(
+        onPanUpdate: (details) => _onPanUpdate(details, widgetSize, 100, 100),
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.blue,
+          child: Center(
+            child: Text(
+              'Drag me',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/shape_move/shape_move.dart
+++ lib/shape_move/shape_move.dart
@@ -1,8 +1,7 @@
 // ref: https://flutter.salon/widget/gesturedetector/#パフォーマンスへの影響
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:wo_read/shape_move/screens/moving_shape.dart';
 
 class ShapeMovePage extends StatefulWidget {
   const ShapeMovePage({super.key});
@@ -12,32 +11,8 @@ class ShapeMovePage extends StatefulWidget {
 }
 
 class _ShapeMovePageState extends State<ShapeMovePage> {
-  Offset position = Offset(0, 0);
-  static const double headerMargin = 80;
-
-  void _onPanUpdate(
-    DragUpdateDetails details,
-    Size widgetSize,
-    double shapeWidth,
-    shapeHeight,
-  ) {
-    final double xPos = max(0, position.dx + details.delta.dx);
-    final double yPos = max(0, position.dy + details.delta.dy);
-
-    setState(() {
-      position = Offset(
-        xPos + shapeWidth <= widgetSize.width ? xPos : position.dx,
-        yPos + shapeHeight + headerMargin <= widgetSize.height
-            ? yPos
-            : position.dy,
-      );
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final Size widgetSize = MediaQuery.of(context).size;
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -45,25 +20,9 @@ class _ShapeMovePageState extends State<ShapeMovePage> {
       ),
       body: Stack(
         children: [
-          Positioned(
-            left: position.dx,
-            top: position.dy,
-            child: GestureDetector(
-              onPanUpdate: (details) =>
-                  _onPanUpdate(details, widgetSize, 100, 100),
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-                child: Center(
-                  child: Text(
-                    'Drag me',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                ),
-              ),
-            ),
-          ),
+          MovingShape(position: Offset(0, 0)),
+          MovingShape(position: Offset(100, 100)),
+          MovingShape(position: Offset(200, 200)),
         ],
       ),
     );
