c43db885df636882054306e77692322c45bf1cff
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-12 16:59:31 +0900

    modify to wrap the Consumer widget

--- lib/shape_move/shape_move.dart
+++ lib/shape_move/shape_move.dart
@@ -1,16 +1,17 @@
 // ref: https://flutter.salon/widget/gesturedetector/#パフォーマンスへの影響
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wo_read/shape_move/screens/moving_shape.dart';
 
-class ShapeMovePage extends StatefulWidget {
+class ShapeMovePage extends ConsumerStatefulWidget {
   const ShapeMovePage({super.key});
 
   @override
   _ShapeMovePageState createState() => _ShapeMovePageState();
 }
 
-class _ShapeMovePageState extends State<ShapeMovePage> {
+class _ShapeMovePageState extends ConsumerState<ShapeMovePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
