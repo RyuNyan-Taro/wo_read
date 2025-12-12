bdeccf5942018d0dff418c042e98e6d0d6de02f8
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-13 05:21:53 +0900

    fix to move shape which is on the tapBox

--- lib/shape_move/shape_move.dart
+++ lib/shape_move/shape_move.dart
@@ -21,7 +21,25 @@ class _ShapeMovePageState extends ConsumerState<ShapeMovePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Shape move'),
       ),
-      body: Stack(children: shapes),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            top: 0,
+            child: InkWell(
+              onTap: () => {print('tapped')},
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                ),
+              ),
+            ),
+          ),
+          ...shapes,
+        ],
+      ),
     );
   }
 }
