58834287abfde19055a8f8cb2e03411c4831bb7f
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-12 16:55:48 +0900

    add initial shapes to test watch the shapes

--- lib/shape_move/providers/shapes_provider.dart
+++ lib/shape_move/providers/shapes_provider.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:wo_read/shape_move/screens/moving_shape.dart';
 
 class Counter extends ChangeNotifier {
-  List<MovingShape> shapes = [];
+  List<MovingShape> shapes = [
+    MovingShape(position: Offset(0, 0)),
+    MovingShape(position: Offset(100, 100)),
+    MovingShape(position: Offset(200, 200)),
+  ];
 
   void addShape(MovingShape shape) {
     shapes.add(shape);
