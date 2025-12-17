666b0d5a4f31d8c5acfe86a81530ec54ff0496c8
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-17 20:23:26 +0900

    apply text base responce to get labels from gemini.

--- lib/record/service/label_service.dart
+++ lib/record/service/label_service.dart
@@ -10,27 +10,31 @@ class LabelService {
   );
 
   Future<LabelResult> getLabels(String text) async {
-    // final prompt =
-    //     "以下のテキストについて以下を返して"
-    //     "\n1. どの感情に一番近いか、happiness、anger、sorrow、pleasureのどれか一つだけを結果として返して"
-    //     "\n2. テンバー式発達スクリーニングテストの以下の分類のどれに一番近いか端的に示して"
-    //     "\n\npersonalSocial、fineMotorAdaptive、language、grossMotor\n\n"
-    //     "*returnの形式は必ず以下とする"
-    //     "happiness、anger、sorrow、pleasureのいずれか, personalSocial、fineMotorAdaptive、language、grossMotorのいずれか"
-    //     "\n\n"
-    //     "「$text」";
-    //
-    // final content = [Content.text(prompt)];
-    // final response = await _gemini.generateContent(content);
-    //
-    // return response.text;
+    final prompt =
+        "以下のテキストについて以下を返して"
+        "\n1. どの感情に一番近いか、happiness、anger、sorrow、pleasureのどれか一つだけを結果として返して"
+        "\n2. テンバー式発達スクリーニングテストの以下の分類のどれに一番近いか端的に示して"
+        "\n\npersonalSocial、fineMotorAdaptive、language、grossMotor\n\n"
+        "*returnの形式は必ず以下とする"
+        "happiness、anger、sorrow、pleasureのいずれか, personalSocial、fineMotorAdaptive、language、grossMotorのいずれか"
+        "\n\n"
+        "「$text」";
 
-    const String responseText = "anger, personalSocial";
+    final content = [Content.text(prompt)];
+    final response = await _gemini.generateContent(content);
+
+    final String? responseText = response.text;
+    if (responseText == null) {
+      return LabelResult(feeling: FeelingType.none, denver: DenverType.none);
+    }
+
     final labels = responseText.split(", ");
+
     final FeelingType feeling = FeelingType.values.firstWhere(
       (e) => e.name == labels[0],
       orElse: () => FeelingType.none,
     );
+
     final DenverType denver = DenverType.values.firstWhere(
       (e) => e.name == labels[1],
       orElse: () => DenverType.none,
