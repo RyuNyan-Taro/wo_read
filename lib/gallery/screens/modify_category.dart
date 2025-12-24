c6136594524ca7d213c3eef3b7979b21427f0d97
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-25 07:20:14 +0900

    modify to show modify_category content

--- lib/gallery/screens/modify_category.dart
+++ lib/gallery/screens/modify_category.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:wo_read/gallery/models/gallery_item.dart';
 
 class ModifyCategoryPage extends StatefulWidget {
@@ -13,7 +13,14 @@ class _ModifyCategoryPageState extends State<ModifyCategoryPage> {
   @override
   // TODO: add category modify page when tap the image
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('modify_category'),
+      ),
+      body: Column(
+        children: [Image.network(widget.gallery.url, width: 100, height: 100)],
+      ),
+    );
   }
 }
