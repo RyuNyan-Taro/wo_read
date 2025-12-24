b6b881bd5fa2458f7dc9be89bad35123cb1adac9
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-24 20:25:42 +0900

    modify to get galleries from the api.

--- lib/gallery/gallery.dart
+++ lib/gallery/gallery.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:wo_read/gallery/service/gallery_service.dart';
 
-class GalleryPage extends StatelessWidget {
+class GalleryPage extends StatefulWidget {
   const GalleryPage({super.key});
+
+  @override
+  State<GalleryPage> createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  List<String>? galleries;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (galleries == null) {
+      _getGalleries();
+    }
+  }
+
+  Future<void> _getGalleries() async {
+    final GalleryService galleryService = GalleryService();
+    final List<String> items = await galleryService.getGalleryUrls();
+
+    setState(() {
+      galleries = items;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
