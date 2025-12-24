0e5b6224dd9bc0fa55e190d4ed687df7dea01698
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-12-24 20:27:24 +0900

    modify to list the api return

--- lib/gallery/gallery.dart
+++ lib/gallery/gallery.dart
@@ -31,15 +31,13 @@ class _GalleryPageState extends State<GalleryPage> {
 
   @override
   Widget build(BuildContext context) {
-    const List<String> galleries = ['testUrl1', 'testUrl2'];
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('gallery'),
       ),
       body: Column(
-        children: galleries.map((gallery) => Text(gallery)).toList(),
+        children: galleries!.map((gallery) => Text(gallery)).toList(),
       ),
     );
   }
