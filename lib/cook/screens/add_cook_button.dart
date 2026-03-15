e38a39eec964d99cd8f6d51eba6162d7a9948657
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-03-15 20:14:55 +0900

    refactor to change add icon for more cooking

--- lib/cook/screens/add_cook_button.dart
+++ lib/cook/screens/add_cook_button.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 import 'add_cook_page.dart';
 
 Widget addCookButton({required BuildContext context, Function? returnAction}) {
-  return FloatingActionButton(
+  return FloatingActionButton.extended(
+    icon: const Icon(Icons.restaurant),
+    label: const Text('記録する'),
+
+    backgroundColor: Colors.orangeAccent,
+    foregroundColor: Colors.white,
+
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+
     onPressed: () async {
-      await Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) {
-            return AddCookPage();
-          },
-        ),
-      );
+      await Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (context) => const AddCookPage()));
       if (returnAction != null) returnAction();
     },
     heroTag: 'addCook',
-    child: Icon(Icons.add_card),
   );
 }
