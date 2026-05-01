59e067b31a818b55a53f18b4766d6f7c366981aa
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-05-01 19:19:17 +0900

    fix icon color

--- lib/cook/screens/add_cook_button.dart
+++ lib/cook/screens/add_cook_button.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'cook_form_page.dart';
+import '../../common/app_theme.dart';
 
 Widget addCookButton({required BuildContext context, Function? returnAction}) {
   return FloatingActionButton.extended(
-    icon: const Icon(Icons.photo_camera),
+    icon: Icon(Icons.photo_camera),
     label: const Text('食事を記録'),
+    foregroundColor: AppTheme.themeData.colorScheme.onPrimary,
     onPressed: () async {
       await Navigator.of(
         context,
